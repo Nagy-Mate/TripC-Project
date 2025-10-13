f36b99526b07e7482acdc663055337435da3c448
GyorgyDavid <gyorgy.david2006@gmail.com>
2025-10-13 18:34:50 +0200

    App

---
+++ Trip/Trip.App/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Trip.App.ViewModels;
+using Trip.Data;
+
+namespace Trip.App
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddDbContext<TripDbContext>();
+
+            //Views
+            builder.Services.AddTransient<MainPage>();
+
+            //ViewModels
+            builder.Services.AddTransient<MainPageViewModel>();
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
