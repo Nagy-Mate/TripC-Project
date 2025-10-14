6dc3cf74ce36450a66ae4fc52cd776480fc906a0
GyorgyDavid <gyorgy.david2006@gmail.com>
2025-10-14 21:07:58 +0200

    Destination POST UI

--- Trip/Trip.App/ViewModels/AddDestinationPageViewModel.cs
+++ Trip/Trip.App/ViewModels/AddDestinationPageViewModel.cs
@@ -1,5 +1,49 @@
-﻿namespace Trip.App.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Net.Http.Json;
+using Trip.Data.DbModels;
 
-public class AddDestinationPageViewModel
+namespace Trip.App.ViewModels;
+
+public partial class AddDestinationPageViewModel : ObservableObject
 {
+    private readonly HttpClient _httpClient;
+
+    [ObservableProperty]
+    private string name;
+
+    [ObservableProperty]
+    private string country;
+
+    [ObservableProperty]
+    private string description;
+
+    public AddDestinationPageViewModel()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    [RelayCommand]
+    private async Task AddDestination()
+    {
+        var newDestination = new Destination
+        {
+            Name = this.Name,
+            Country = this.Country,
+            Description = this.Description,
+        };
+
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync("http://localhost:5048/api/destinations", newDestination);
+
+            Name = string.Empty;
+            Country = string.Empty;
+            Description = string.Empty;
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine($"Hiba az adatok küldésekor: {ex.Message}");
+        }
+    }
 }
