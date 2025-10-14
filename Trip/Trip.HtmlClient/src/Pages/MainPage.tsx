af844615de259c686f3430df142bd8951483317f
Nagy Máté <morsakk.mate@gmail.com>
2025-10-14 22:11:20 +0200

    filter by destination

--- Trip/Trip.HtmlClient/src/Pages/MainPage.tsx
+++ Trip/Trip.HtmlClient/src/Pages/MainPage.tsx
@@ -9,13 +9,21 @@ function MainPage() {
   const [trips, setTrips] = useState<Array<Trip>>([]);
   const [destinations, setDestinations] = useState<Array<Destination>>([]);
   const [tripToDeleteId, setTripToDeleteId] = useState<number | null>(null);
+  const [filterDestId, setFilterDestId] = useState<number>(0);
 
   useEffect(() => {
-    apiClient
-      .get("/trips")
-      .then((res) => setTrips(res.data))
-      .catch((e) => console.error(e));
-  }, [tripToDeleteId]);
+    if (filterDestId === 0) {
+      apiClient
+        .get("/trips")
+        .then((res) => setTrips(res.data))
+        .catch((e) => console.error(e));
+    } else if (destinations.find((d) => d.id == filterDestId)) {
+      apiClient
+        .get(`/trips/byDestination/${filterDestId}`)
+        .then((res) => setTrips(res.data))
+        .catch((e) => console.error(e));
+    }
+  }, [tripToDeleteId, filterDestId]);
 
   useEffect(() => {
     apiClient
@@ -37,38 +45,62 @@ function MainPage() {
       <Link to="/create">
         <button id="createBtn">Create</button>
       </Link>
-      <table>
-        <tbody>
-          <tr>
-            <th id="fth">Id</th>
-            <th>Name</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Destination</th>
-            <th>Actions</th>
-          </tr>
-          {trips.map((t) => (
-            <tr key={t.id}>
-              <td>{t.id}</td>
-              <td>{t.name}</td>
-              <td>{new Date(t.startDate).toLocaleDateString()}</td>
-              <td>{new Date(t.endDate).toLocaleDateString()}</td>
-              <td>{destinations.find((d) => d.id == t.destinationId)?.name}</td>
-              <td>
-                <button
-                  className="dButton"
-                  onClick={() => setTripToDeleteId(t.id)}
-                >
-                  Delete
-                </button>
-                <Link to={`/updateTirp/${t.id}`}>
-                  <button className="uButton">Update</button>
-                </Link>
-              </td>
-            </tr>
+      <p>
+        <b>Filter By Destinations: </b>
+        <select
+          value={filterDestId || 0}
+          onChange={(e) => {
+            setFilterDestId(Number(e.target.value));
+          }}
+        >
+          <option value={0}>All</option>
+          {destinations.map((d) => (
+            <option key={d.id} value={d.id}>
+              {d.name}
+            </option>
           ))}
-        </tbody>
-      </table>
+        </select>
+      </p>
+      {trips.length != 0 ? (
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th id="fth">Id</th>
+                <th>Name</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Destination</th>
+                <th>Actions</th>
+              </tr>
+              {trips.map((t) => (
+                <tr key={t.id}>
+                  <td>{t.id}</td>
+                  <td>{t.name}</td>
+                  <td>{new Date(t.startDate).toLocaleDateString()}</td>
+                  <td>{new Date(t.endDate).toLocaleDateString()}</td>
+                  <td>
+                    {destinations.find((d) => d.id == t.destinationId)?.name}
+                  </td>
+                  <td>
+                    <button
+                      className="dButton"
+                      onClick={() => setTripToDeleteId(t.id)}
+                    >
+                      Delete
+                    </button>
+                    <Link to={`/updateTirp/${t.id}`}>
+                      <button className="uButton">Update</button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="inputErr"> <h2>No trip</h2> </div>
+      )}
     </>
   );
 }
